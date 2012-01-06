77b560005f6e872d9057b4ff2be5605467987404
Jesse Rao <jesse.rao@liferay.com>
2012-01-06 12:29:29 -0500

    added new liferay doc utils

---
+++ code/liferay-doc-utils/src/com/liferay/documentation/util/DocUtils.java
@@ -0,0 +1,58 @@
+package com.liferay.documentation.util;
+
+import java.io.IOException;
+
+public class DocUtils {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String command = args[0];
+		if (args[0] == null) {
+			System.out.println("Please specify an argument: NumberImages, ResetImages, or AddMarkdownMetadata");
+		} 
+		else {
+			if (command.equalsIgnoreCase("NumberImages")) {
+				String markdownFilePath = args[1];
+				if (markdownFilePath != null) {
+					try {
+						NumberImages.numberImages(markdownFilePath);
+					} catch (IOException e) {
+						System.out.println("IOException: " + e.getMessage());
+					}
+				}
+			}
+			else if (command.equalsIgnoreCase("ResetImages")) {
+				String markdownFilePath = args[1];
+				if (markdownFilePath != null) {
+					try {
+						ResetImages.resetImages(markdownFilePath);
+					} catch (IOException e) {
+						System.out.println("IOException: " + e.getMessage());
+					}
+				}
+			}
+			else if (command.equalsIgnoreCase("AddMarkdownMetadata")) {
+				String markdownFilePath = args[1];
+				String title = args[2];
+				String author = args[3];
+				String date = args[4];
+				if (markdownFilePath != null) {
+					try {
+						AddMarkdownMetadata.addMarkdownMetadata(markdownFilePath, title, author, date);
+					} catch (IOException e) {
+						System.out.println("IOException: " + e.getMessage());
+					}
+				}
+			}
+			else {
+				System.out.println("You entered an invalid argument. Please try again. You can use NumberImages, ResetImages, or AddMarkdownMetadata");
+			}
+			
+		}
+
+	}
+
+}
