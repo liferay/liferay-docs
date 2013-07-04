7a82ba78866deef33a81d37c5e76ef49b37516d5
Jesse Rao <jesse.rao@liferay.com>
2013-07-03 21:03:29 -0400

    LRDOCS-523 Updated Event Listing and Location Listing portlets not to use an entityFromRequest method; fixed createDate implementation

--- devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/EventListingPortlet.java
+++ devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/EventListingPortlet.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletRequest;
 
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -18,37 +17,6 @@ public class EventListingPortlet extends MVCPortlet {
 
 	public void addEvent(ActionRequest request, ActionResponse response)
 			throws Exception {
-
-		Event event = eventFromRequest(request);
-
-		EventLocalServiceUtil.addEvent(event);
-
-		sendRedirect(request, response);
-	}
-	
-	public void updateEvent(ActionRequest request, ActionResponse response)
-		throws Exception {
-		
-		long eventId = ParamUtil.getLong(request, "eventId");
-
-		Event event = EventLocalServiceUtil.fetchEvent(eventId);
-
-		EventLocalServiceUtil.updateEvent(event);
-
-		sendRedirect(request, response);
-	}
-
-	public void deleteEvent(ActionRequest request, ActionResponse response)
-		throws Exception {
-
-		long eventId = ParamUtil.getLong(request, "eventId");
-
-		EventLocalServiceUtil.deleteEvent(eventId);
-
-		sendRedirect(request, response);
-	}	
-
-	private Event eventFromRequest(PortletRequest request) {
 
 		EventImpl event = new EventImpl();
 
@@ -66,8 +34,47 @@ public class EventListingPortlet extends MVCPortlet {
 		event.setUserId(themeDisplay.getUserId());
 		event.setCompanyId(themeDisplay.getCompanyId());
 		event.setGroupId(themeDisplay.getScopeGroupId());
-		
-		return event;
+
+		EventLocalServiceUtil.addEvent(event);
+
+		sendRedirect(request, response);
 	}
+	
+	public void updateEvent(ActionRequest request, ActionResponse response)
+		throws Exception {
+		
+		long eventId = ParamUtil.getLong(request, "eventId");
+
+		Event event = EventLocalServiceUtil.fetchEvent(eventId);
+		
+		event.setEventId(ParamUtil.getLong(request, "eventId"));
+		event.setName(ParamUtil.getString(request, "name"));
+		event.setDescription(ParamUtil.getString(request, "description"));
+		event.setLocationId(ParamUtil.getLong(request, "locationId"));
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat();
+		event.setDate(ParamUtil.getDate(request, "date", dateFormat));
+		
+		ThemeDisplay themeDisplay = (ThemeDisplay) request
+				.getAttribute(WebKeys.THEME_DISPLAY);
+
+		event.setUserId(themeDisplay.getUserId());
+		event.setCompanyId(themeDisplay.getCompanyId());
+		event.setGroupId(themeDisplay.getScopeGroupId());	
+
+		EventLocalServiceUtil.updateEvent(event);
+
+		sendRedirect(request, response);
+	}
+
+	public void deleteEvent(ActionRequest request, ActionResponse response)
+		throws Exception {
+
+		long eventId = ParamUtil.getLong(request, "eventId");
+
+		EventLocalServiceUtil.deleteEvent(eventId);
+
+		sendRedirect(request, response);
+	}	
 
 }
--- devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/LocationListingPortlet.java
+++ devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/LocationListingPortlet.java
@@ -2,7 +2,6 @@ package com.nosester.portlet.eventlisting;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletRequest;
 
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -16,37 +15,6 @@ public class LocationListingPortlet extends MVCPortlet {
 
 	public void addLocation(ActionRequest request, ActionResponse response)
 			throws Exception {
-
-		Location location = locationFromRequest(request);
-
-		LocationLocalServiceUtil.addLocation(location);
-
-		sendRedirect(request, response);
-	}
-	
-	public void updateLocation(ActionRequest request, ActionResponse response)
-		throws Exception {
-
-		long locationId = ParamUtil.getLong(request, "locationId");
-		
-		Location location = LocationLocalServiceUtil.fetchLocation(locationId);
-
-		LocationLocalServiceUtil.updateLocation(location);
-
-		sendRedirect(request, response);
-	}
-
-	public void deleteLocation(ActionRequest request, ActionResponse response)
-		throws Exception {
-
-		long locationId = ParamUtil.getLong(request, "locationId");
-
-		LocationLocalServiceUtil.deleteLocation(locationId);
-
-		sendRedirect(request, response);
-	}	
-
-	private Location locationFromRequest(PortletRequest request) {
 
 		LocationImpl location = new LocationImpl();
 
@@ -64,8 +32,47 @@ public class LocationListingPortlet extends MVCPortlet {
 		location.setUserId(themeDisplay.getUserId());
 		location.setCompanyId(themeDisplay.getCompanyId());
 		location.setGroupId(themeDisplay.getScopeGroupId());
-		
-		return location;
+
+		LocationLocalServiceUtil.addLocation(location);
+
+		sendRedirect(request, response);
 	}
+	
+	public void updateLocation(ActionRequest request, ActionResponse response)
+		throws Exception {
+
+		long locationId = ParamUtil.getLong(request, "locationId");
+		
+		Location location = LocationLocalServiceUtil.fetchLocation(locationId);
+		
+		location.setLocationId(ParamUtil.getLong(request, "locationId"));
+		location.setName(ParamUtil.getString(request, "name"));
+		location.setDescription(ParamUtil.getString(request, "description"));
+		location.setStreetAddress(ParamUtil.getString(request, "streetAddress"));
+		location.setCity(ParamUtil.getString(request, "city"));
+		location.setStateOrProvince(ParamUtil.getString(request, "stateOrProvince"));
+		location.setCountry(ParamUtil.getString(request, "country"));
+		
+		ThemeDisplay themeDisplay = (ThemeDisplay) request
+				.getAttribute(WebKeys.THEME_DISPLAY);
+
+		location.setUserId(themeDisplay.getUserId());
+		location.setCompanyId(themeDisplay.getCompanyId());
+		location.setGroupId(themeDisplay.getScopeGroupId());
+		
+		LocationLocalServiceUtil.updateLocation(location);
+
+		sendRedirect(request, response);
+	}
+
+	public void deleteLocation(ActionRequest request, ActionResponse response)
+		throws Exception {
+
+		long locationId = ParamUtil.getLong(request, "locationId");
+
+		LocationLocalServiceUtil.deleteLocation(locationId);
+
+		sendRedirect(request, response);
+	}	
 
 }
