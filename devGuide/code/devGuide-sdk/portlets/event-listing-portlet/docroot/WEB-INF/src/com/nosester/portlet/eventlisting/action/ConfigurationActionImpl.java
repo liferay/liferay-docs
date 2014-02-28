fab816b46988e74f95af3dda36ab429fbef7df86
Cody Hoag <cody.hoag@liferay.com>
2014-02-28 17:06:21 -0500

    LRDOCS-949 SF

--- devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/action/ConfigurationActionImpl.java
+++ devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/action/ConfigurationActionImpl.java
@@ -1,27 +1,43 @@
-	package com.nosester.portlet.eventlisting.action;
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
-    import javax.portlet.ActionRequest;
-    import javax.portlet.ActionResponse;
-    import javax.portlet.PortletConfig;
-    import javax.portlet.PortletPreferences;
+package com.nosester.portlet.eventlisting.action;
 
-    import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
 
-    public class ConfigurationActionImpl extends DefaultConfigurationAction {
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletPreferences;
 
-        @Override
-        public void processAction(
-            PortletConfig portletConfig, ActionRequest actionRequest,
-            ActionResponse actionResponse) throws Exception {  
+/**
+ * @author Joe Bloggs
+ */
+public class ConfigurationActionImpl extends DefaultConfigurationAction {
 
-            super.processAction(portletConfig, actionRequest, actionResponse);
+	@Override
+	public void processAction(
+		PortletConfig portletConfig, ActionRequest actionRequest,
+		ActionResponse actionResponse) throws Exception {  
 
-            PortletPreferences prefs = actionRequest.getPreferences();
+		super.processAction(portletConfig, actionRequest, actionResponse);
 
-            String showLocationAddress = prefs.getValue(
-                "showLocationAddress", "true");
+		PortletPreferences prefs = actionRequest.getPreferences();
 
-            System.out.println("showLocationAddress=" + showLocationAddress +
-                " in ConfigurationActionImpl.processAction().");
-        }
-    }
+		String showLocationAddress = prefs.getValue("showLocationAddress", "true");
+
+		System.out.println("showLocationAddress=" + showLocationAddress +
+			" in ConfigurationActionImpl.processAction().");
+	}
+}
