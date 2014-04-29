4aa90700c66451e06b5f7919d81291d3aa4b8d8e
Cody Hoag <cody.hoag@liferay.com>
2014-04-28 23:40:37 -0400

    LRDOCS-346 Update Location Listing code to incorporate ADT configuration

--- devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/template/LocationListingPortletDisplayTemplateHandler.java
+++ devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/template/LocationListingPortletDisplayTemplateHandler.java
@@ -1,65 +1,52 @@
 package com.nosester.portlet.eventlisting.template;
 
-import com.liferay.portal.kernel.configuration.Filter;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
 import com.liferay.portal.kernel.template.TemplateVariableGroup;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portlet.portletdisplaytemplate.util.PortletDisplayTemplateConstants;
-import com.liferay.util.portlet.PortletProps;
-
 import com.nosester.portlet.eventlisting.model.Location;
 import com.nosester.portlet.eventlisting.util.PortletKeys;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-public class LocationListingPortletDisplayTemplateHandler
-		extends BasePortletDisplayTemplateHandler {
+public class LocationListingPortletDisplayTemplateHandler extends
+        BasePortletDisplayTemplateHandler {
 
-	public String getClassName() {
-		return Location.class.getName();
-	}
+    public String getClassName() {
+        return Location.class.getName();
+    }
 
-	public String getName(Locale locale) {
-		String locations = LanguageUtil.get(locale, PortletKeys.LOCATIONS);
+    public String getName(Locale locale) {
+        String locations = LanguageUtil.get(locale, "locations");
 
-		return locations.concat(StringPool.SPACE).concat(
-				LanguageUtil.get(locale, "template"));
-	}
+        return locations.concat(StringPool.SPACE).concat(
+                LanguageUtil.get(locale, "template"));
+    }
 
-	public String getResourceName() {
-		return PortletKeys.LOCATION_LISTING_PORTLET_ID;
-	}
+    public String getResourceName() {
+        return PortletKeys.LOCATION_LISTING_PORTLET_ID;
+    }
 
-	public String getTemplatesHelpPath(String language) {
-		return PortletProps.get(
-			getTemplatesHelpPropertyKey(), new Filter(language));
-	}
+    @Override
+    public Map<String, TemplateVariableGroup> getTemplateVariableGroups(
+            long classPK, String language, Locale locale)
+        throws Exception {
 
-	@Override
-	public String getTemplatesHelpPropertyKey() {
-		return "locations.portlet.display.templates.help";
-	}
+        Map<String, TemplateVariableGroup> templateVariableGroups =
+            super.getTemplateVariableGroups(classPK, language, locale);
 
-	@Override
-	public Map<String, TemplateVariableGroup> getTemplateVariableGroups(
-			long classPK, String language, Locale locale)
-		throws Exception {
+        TemplateVariableGroup templateVariableGroup =
+            templateVariableGroups.get("fields");
 
-		Map<String, TemplateVariableGroup> templateVariableGroups =
-			super.getTemplateVariableGroups(classPK, language, locale);
+        templateVariableGroup.empty();
 
-		TemplateVariableGroup templateVariableGroup =
-			templateVariableGroups.get("fields");
+        templateVariableGroup.addCollectionVariable(
+            "locations", List.class, PortletDisplayTemplateConstants.ENTRIES,
+            "location", Location.class, "curLocation", "name");
 
-		templateVariableGroup.empty();
-
-		templateVariableGroup.addCollectionVariable(
-			"locations", List.class, PortletKeys.LOCATIONS,
-			"location", Location.class, "curLocation", "name");
-
-		return templateVariableGroups;
-	}
-
+        return templateVariableGroups;
+    }
 }
