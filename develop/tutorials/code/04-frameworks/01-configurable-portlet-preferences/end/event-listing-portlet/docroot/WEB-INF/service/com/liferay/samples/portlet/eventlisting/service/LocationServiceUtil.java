6de12d792affc12105e8b155b3c84c205d74d187
Cody Hoag <cody.hoag@liferay.com>
2014-05-01 16:30:41 -0400

    LRDOCS-1004 Add Portlet Prefs begin/end portlet code

---
+++ develop/tutorials/code/04-frameworks/01-configurable-portlet-preferences/end/event-listing-portlet/docroot/WEB-INF/service/com/liferay/samples/portlet/eventlisting/service/LocationServiceUtil.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.samples.portlet.eventlisting.service;
+
+import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+import com.liferay.portal.service.InvokableService;
+
+/**
+ * Provides the remote service utility for Location. This utility wraps
+ * {@link com.liferay.samples.portlet.eventlisting.service.impl.LocationServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author Joe Bloggs
+ * @see LocationService
+ * @see com.liferay.samples.portlet.eventlisting.service.base.LocationServiceBaseImpl
+ * @see com.liferay.samples.portlet.eventlisting.service.impl.LocationServiceImpl
+ * @generated
+ */
+public class LocationServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link com.liferay.samples.portlet.eventlisting.service.impl.LocationServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public static java.lang.String getBeanIdentifier() {
+		return getService().getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public static void setBeanIdentifier(java.lang.String beanIdentifier) {
+		getService().setBeanIdentifier(beanIdentifier);
+	}
+
+	public static java.lang.Object invokeMethod(java.lang.String name,
+		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
+		throws java.lang.Throwable {
+		return getService().invokeMethod(name, parameterTypes, arguments);
+	}
+
+	public static void clearService() {
+		_service = null;
+	}
+
+	public static LocationService getService() {
+		if (_service == null) {
+			InvokableService invokableService = (InvokableService)PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(),
+					LocationService.class.getName());
+
+			if (invokableService instanceof LocationService) {
+				_service = (LocationService)invokableService;
+			}
+			else {
+				_service = new LocationServiceClp(invokableService);
+			}
+
+			ReferenceRegistry.registerReference(LocationServiceUtil.class,
+				"_service");
+		}
+
+		return _service;
+	}
+
+	/**
+	 * @deprecated As of 6.2.0
+	 */
+	public void setService(LocationService service) {
+	}
+
+	private static LocationService _service;
+}
