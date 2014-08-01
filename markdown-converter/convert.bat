9b2649667321ac3bdb2a1cc2522e06031d98d202
Jesse Rao <jesse.rao@liferay.com>
2014-08-01 18:55:36 -0400

    LRDOCS-1312 Added .bat script for running Markdown converter for Windows. Updated usage message of convert.sh

---
+++ markdown-converter/convert.bat
@@ -0,0 +1,18 @@
+if "%1"=="" goto usage
+if "%2"=="" goto usage
+
+java.exe -cp .;..\lib\* com.liferay.documentation.util.MarkdownToHtmlMain %1 %2
+
+goto end
+
+:usage
+
+echo Usage: convert.bat [Markdown file to convert] [HTML file to be written]
+
+echo The fist argument is the path to the Markdown file to convert to HTML.
+
+echo The second argument is the path to the HTML file to be created.
+
+goto end
+
+:end
--- markdown-converter/convert.sh
+++ markdown-converter/convert.sh
@@ -2,11 +2,11 @@
 
 if [ $# -lt 1 ]; then
 	echo
-	echo Usage: ./convert.sh article.markdown or ./convert.sh article.markdown article.html
+	echo Usage: ./convert.sh \[Markdown file to convert\] or ./convert.sh \[Markdown file to convert\] \[HTML file to be written\]
 	echo
-	echo The first argument, article.markdown, is the path to the Markdown file to convert to HTML.
+	echo The first argument is the path to the Markdown file to convert to HTML.
 	echo
-	echo The second argument, article.html, is optional. It specifies the path to the HTML file to be created. If this argument is omitted, the HTML file to be created will be created in the same directory as the Markdown file and will have the same filename as the Markdown file except that the .markdown file extension will be replaced by the .html file extension.  
+	echo The second argument is optional. It specifies the path to the HTML file to be created. If this argument is omitted, the HTML file to be created will be created in the same directory as the Markdown file and will have the same filename as the Markdown file except that the .markdown file extension will be replaced by the .html file extension.  
 	echo
 	exit 1
 elif [ $# -lt 2 ]; then
