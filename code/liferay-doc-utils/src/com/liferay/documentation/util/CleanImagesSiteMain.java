6fbad300c07e0b8db82bc8c4df9bb9c1aa268f07
Russell Bohl <russell.bohl@liferay.com>
2014-09-08 14:20:26 -0400

    Created the class for cleaning unused site images from the liferay-docs repository.

---
+++ code/liferay-doc-utils/src/com/liferay/documentation/util/CleanImagesSiteMain.java
@@ -0,0 +1,9 @@
+package com.liferay.documentation.util;
+
+public class CleanImagesSiteMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start checking for unused images ...");
+	}
+}
