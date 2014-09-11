9678c08042f82e2a5c58467869c2035f64ba9378
Russell Bohl <russell.bohl@liferay.com>
2014-09-11 13:37:12 -0400

    Removed comments from class, formatted.

--- code/liferay-doc-utils/src/com/liferay/documentation/util/CleanImagesSiteMain.java
+++ code/liferay-doc-utils/src/com/liferay/documentation/util/CleanImagesSiteMain.java
@@ -11,34 +11,23 @@ public class CleanImagesSiteMain {
 
 		System.out.println("Start checking for unused images ...");
 
-		// 1. Store the list of the target directory's files (really
-		// directories) in an array.
-
 		String docDir = args[0];
 
 		File articleDir = new File("../" + docDir + "/articles");
 
 		File[] articleDirs = articleDir.listFiles();
 
-		// 2. Loop through the array, and store the name of each directory in a
-		// File Array.
-
 		ArrayList<String> imageNames = new ArrayList<String>();
 
 		for (File topicDir : articleDirs) {
 			if (topicDir.isDirectory()) {
-				
+
 				File[] topicDirs = topicDir.listFiles();
-				
+
 				System.out.println("checking " + topicDir.getPath());
 
-				// 3. This gets me the image names into an
-				// array list (not surprisingly, named imageNames, instantiated
-				// above). It extracts the imageNames from line using a private static
-				// method, defined below the Main method.
-
 				for (File mkdFile : topicDirs) {
-					
+
 					System.out.println(" " + mkdFile.getName());
 					if (!mkdFile.getName().endsWith("markdown")) {
 						continue;
@@ -56,10 +45,7 @@ public class CleanImagesSiteMain {
 				}
 			}
 		}
-		// 4. Save the image files (in the images directory of the docDir) in an
-		// array so we can check the imageNames array against them. Delete
-		// any imageFile that is not found in the imageNames array. 
-		
+
 		File imagesDir = new File("../" + docDir + "/images");
 		File[] imageFiles = imagesDir.listFiles();
 
@@ -72,9 +58,6 @@ public class CleanImagesSiteMain {
 		}
 	}
 
-	// 5. Below is the method we're using to extract the image Name from the line
-	// containing image references in the markdown file.
-	
 	private static String extractImageName(String line) {
 
 		String imageName = line.substring(
