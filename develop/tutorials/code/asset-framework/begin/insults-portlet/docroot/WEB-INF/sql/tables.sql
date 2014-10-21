66641cf1a901c7c70bc5444d78c4b76fb740cccf
ngaskill <nicholas.gaskill@liferay.com>
2014-10-21 16:52:02 -0400

    Removing old asset framework Insults portlet for begin state.

--- develop/tutorials/code/asset-framework/begin/insults-portlet/docroot/WEB-INF/sql/tables.sql
+++
@@ -1,9 +0,0 @@
-create table INSULT_Insult (
-	uuid_ VARCHAR(75) null,
-	insultId LONG not null primary key,
-	insultString VARCHAR(75) null,
-	userId LONG,
-	groupId LONG,
-	companyId LONG,
-	userName VARCHAR(75) null
-);
