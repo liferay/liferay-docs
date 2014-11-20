687e4a8099e9511a923c4749ca150bcfb3cc87e2
Jesse Rao <jesse.rao@liferay.com>
2014-11-20 15:26:10 -0500

    LRDOCS-1511 Implement the existing User Guide figure numbering behavior for Discover/Portal and Discover/Deployment on dev.liferay.com

---
+++ code/liferay-doc-utils/src/com/liferay/documentation/util/ResetImagesDiscover.java
@@ -0,0 +1,68 @@
+package com.liferay.documentation.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.FileUtils;
+
+public class ResetImagesDiscover {
+
+	public static void resetImages(String markdownFilePath) throws IOException {
+		File markdownfile = new File(markdownFilePath); // example: "/home/$USER/workspace/01-example-chapter.markdown"
+		
+		String[] markdownFilePathArray = markdownFilePath.split("/");
+		
+		int markdownFilePathArrayLength = markdownFilePathArray.length;
+		
+		String parentFolderName = "";
+		
+		if (markdownFilePathArrayLength < 2) {
+			System.out.println("No chapter number could be extracted from the"
+			+ "Markdown file path since the parent folder of the Markdown file"
+			+ "could not be found.");
+		}
+		else {
+			parentFolderName = markdownFilePathArray[markdownFilePathArrayLength - 2];
+		}
+		
+		String chapternum = parentFolderName.substring(0,2);
+		System.out.println("chapternum: " + chapternum);
+		int chnum = Integer.parseInt(chapternum);
+		System.out.println("chnum: " + chnum);
+
+		String source = FileUtils.readFileToString(markdownfile);
+		String find = "\\!\\[Figure " + "[0-9][0-9]?" + "\\.[0-9[x]]:";
+		System.out.println("find: " + find);
+		String replace = "\\!\\[Figure " + chnum + ".x:";
+		System.out.println("replace: " + replace);
+
+		Pattern pattern = Pattern.compile(find);
+		Matcher matcher = pattern.matcher(source);
+		String output = matcher.replaceAll(replace);
+
+		find = "\\!\\[Figure " + "[0-9][0-9]?" + "\\.[0-9][0-9[x]]:";
+		System.out.println("find: " + find);
+		pattern = Pattern.compile(find);
+		matcher = pattern.matcher(output);
+		output = matcher.replaceAll(replace);
+		System.out.println("replace: " + replace);
+
+		//
+		// Saves the updated file, replaces the original
+		//
+		FileUtils.writeStringToFile(markdownfile, output);
+
+		/*
+		 * This code is for saving the output to a new file so the original is
+		 * not overwritten; useful for debugging
+		 * 
+		 * String markdownfileoutputpath = args[1]; File markdownfileoutput =
+		 * new File(markdownfileoutputpath);
+		 * FileUtils.writeStringToFile(markdownfileoutput, output);
+		 */
+
+	}
+
+}
