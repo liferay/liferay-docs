bcdb8052237aabb26886cb86820e72b38093653d
Rich Sezov <richard.sezov@liferay.com>
2014-12-01 17:00:51 -0500

    Script to fix generated LaTeX code.

---
+++ code/liferay-book-utils/src/fix-latex.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# encoding: utf-8
+'''
+ -- 
+
+ is a description
+
+It defines classes_and_methods
+
+@author:     Rich Sezov
+
+@copyright:  2014 Liferay, Inc. All rights reserved.
+
+@license:    license
+
+'''
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("output", help="The file name to be written")
+parser.add_argument("file", help="file to be fixed")
+
+args = parser.parse_args()
+
+outfile = open(args.output, "w")
+infile = open(args.file, "r")
+content = infile.readlines()
+
+for i in content:
+    
+    if i.startswith("\caption"):
+        eff = i.find("F")
+        prefix = i[:eff]
+        suffix = i[eff:]
+        i = prefix + "\\" + "\\" + suffix
+    
+    #if i.startswith("\href"):
+    #    print (i)
+    #    print("Found href; ignoring.")
+    #    i = "}"
+
+    outfile.write(i)     
+        
