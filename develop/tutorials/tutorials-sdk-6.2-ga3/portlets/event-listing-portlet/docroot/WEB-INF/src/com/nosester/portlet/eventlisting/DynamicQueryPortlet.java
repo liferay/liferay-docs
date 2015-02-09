e59e6b7396797b92974d56217382f405251b6dfb
Jesse Rao <jesse.rao@liferay.com>
2015-02-09 16:04:32 -0500

    LRDOCS-1573 Copy event-listing-portlet project from liferay-docs/develop/tutorials/code/svc-build/ to liferay-docs/develop/tutorials/tutorials-sdk

---
+++ develop/tutorials/tutorials-sdk-6.2-ga3/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/portlet/eventlisting/DynamicQueryPortlet.java
@@ -0,0 +1,11 @@
+package com.nosester.portlet.eventlisting;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class DynamicQueryPortlet
+ */
+public class DynamicQueryPortlet extends MVCPortlet {
+ 
+
+}
