60fc71185e1cd18609ffb48197c2df889e0a00a1
Jesse Rao <jesse.rao@liferay.com>
2015-02-11 10:48:41 -0500

    LRDOCS-1573 Remove Event Listing portlet as part of refactorization

--- develop/tutorials/tutorials-sdk-6.2-ga3/portlets/event-listing-portlet/docroot/WEB-INF/src/com/liferay/docs/eventlisting/util/PortletKeys.java
+++
@@ -1,8 +0,0 @@
-package com.liferay.docs.eventlisting.util;
-
-public class PortletKeys extends com.liferay.portal.util.PortletKeys {
-
-	public static final String LOCATIONS = "locations";
-
-	public final static String LOCATION_LISTING_PORTLET_ID = "locationlisting_WAR_eventlistingportlet";
-}
