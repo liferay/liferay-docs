9a7306af046b66d0ca79be3260d185a27c630801
Rich Sezov <richard.sezov@liferay.com>
2015-06-04 17:48:31 -0400

    Initial 7.0 Plugins SDK.

---
+++ develop/tutorials/code/liferay-plugins-sdk-7.0.0/portlets/create.bat
@@ -0,0 +1,33 @@
+@echo off
+
+if "" == "%1" goto errorCreate
+
+if "" == "%2" goto errorCreate
+
+if "" == "%3" set PORTLET_FRAMEWORK=mvc
+if not "" == "%3" set PORTLET_FRAMEWORK=%3
+
+call ..\tools\gradle\gradlew.bat --build-file=..\sdk.gradle -PportletName=%1 -PportletDisplayName=%2 -PportletFramework=%PORTLET_FRAMEWORK% createPortlet
+
+rem call ant deploy
+
+goto end
+
+:errorCreate
+	echo.
+	echo Usage: create.bat hello-world "Hello World"
+	echo.
+	echo The first hello-world is your portlet id. A new directory will be created based
+	echo on the portlet id.
+	echo.
+	echo The second "Hello World" is the portlet's display name. The quotation marks are
+	echo only needed because there is a space in the display name.
+	echo.
+	echo A third value can be passed to specify the portlet framework to use. Valid
+	echo values are "jsf", "icefaces", "liferay_faces_alloy", "mvc", "primefaces",
+	echo "richfaces", "spring_mvc", or "vaadin". The default value is "mvc". The
+	echo quotation marks are not needed.
+
+	goto end
+
+:end
