b6a5c1d9e0e7246f2b3e81fd37ddaba2dc0b1b43
Jesse Rao <jesse.rao@liferay.com>
2015-07-17 17:59:29 -0400

    LRDOCS-1807 Add Example JSP Portlet to tutorials SDK; demonstrate how to enable JSP and the JSTL and AlloyUI tag libraries

--- develop/tutorials/code/liferay-plugins-sdk-7.0.0/portlets/example-jsp-portlet/src/com/liferay/docs/examplejspportlet/ExampleJSPPortlet.java
+++ develop/tutorials/code/liferay-plugins-sdk-7.0.0/portlets/example-jsp-portlet/src/com/liferay/docs/examplejspportlet/ExampleJSPPortlet.java
@@ -1,15 +1,10 @@
 package com.liferay.docs.examplejspportlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.portlet.GenericPortlet;
 import javax.portlet.Portlet;
-import javax.portlet.PortletException;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 @Component(
     immediate = true,
@@ -17,19 +12,12 @@ import org.osgi.service.component.annotations.Component;
         "com.liferay.portlet.display-category=category.sample",
         "com.liferay.portlet.instanceable=true",
         "javax.portlet.display-name=Example JSP Portlet",
+        "javax.portlet.init-param.template-path=/",
+        "javax.portlet.init-param.view-template=/view.jsp",
         "javax.portlet.security-role-ref=power-user,user"
     },
     service = Portlet.class
 )
-public class ExampleJSPPortlet extends GenericPortlet {
+public class ExampleJSPPortlet extends MVCPortlet {
         
-	@Override
-    protected void doView(RenderRequest request, RenderResponse response)
-        throws PortletException, IOException {
-
-        PrintWriter printWriter = response.getWriter();
-
-        printWriter.print("Example JSP Portlet is working!");
-    }
-
 }
