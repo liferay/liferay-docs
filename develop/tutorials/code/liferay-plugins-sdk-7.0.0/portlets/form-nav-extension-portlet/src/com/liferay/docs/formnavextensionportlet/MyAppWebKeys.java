0fb425f749e932d1ef9bd9c6cce073886272c3d5
Jim Hinkey <james.hinkey@liferay.com>
2015-09-26 13:46:44 -0400

    Move self sufficient form-nav-extension-portlet to code/osgi/modules/ and rebuild bundle JAR

--- develop/tutorials/code/liferay-plugins-sdk-7.0.0/portlets/form-nav-extension-portlet/src/com/liferay/docs/formnavextensionportlet/MyAppWebKeys.java
+++
@@ -1,8 +0,0 @@
-package com.liferay.docs.formnavextensionportlet;
-
-public class MyAppWebKeys {
-
-	public static final String COMPANY_MY_APP_FEATURE_ENABLED =
-		"COMPANY_MY_APP_FEATURE_ENABLED";
-
-}
