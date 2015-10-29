0195524320ac0a4798511f57dfe2996896c14d2c
Jesse Rao <jesse.rao@liferay.com>
2015-10-29 12:40:20 -0400

    LRDOCS-1978 Use service tracker instead of getting service directly from service reference

--- discover/deployment/code/script-util.groovy
+++ discover/deployment/code/script-util.groovy
@@ -3,12 +3,13 @@ import com.liferay.docs.scriptutil.api.ScriptUtil;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 Bundle bundle = FrameworkUtil.getBundle(ScriptUtil.class);
 BundleContext context = bundle.getBundleContext();
-ServiceReference ref = context.getServiceReference(ScriptUtil.class.getName());
-ScriptUtil util = context.getService(ref);
+ServiceTracker tracker = new ServiceTracker(context, ScriptUtil.class.getName(), null);
+tracker.open();
+ScriptUtil util = tracker.getService();
 
 out.println(util.operationOne());
-out.println(util.operationTwo("Joe"));
+out.println(util.operationTwo("Joe Bloggs"));
