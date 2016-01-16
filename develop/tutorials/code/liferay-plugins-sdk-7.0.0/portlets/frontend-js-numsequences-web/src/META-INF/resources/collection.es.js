bdd2a71633a86337e06db541486c8ca2481fc913
Mike Williams <michael.williams@liferay.com>
2016-01-15 19:09:57 -0500

    LRDOCS-1852 removed old code examples.

--- develop/tutorials/code/liferay-plugins-sdk-7.0.0/portlets/frontend-js-numsequences-web/src/META-INF/resources/collection.es.js
+++
@@ -1,9 +0,0 @@
-import F from './sequences/fibonacci.es';
-import P from './sequences/prime.es';
-import S from './sequences/square.es';
-import T from './sequences/triangle.es';
-
-export { F as fibonacci };
-export { P as prime };
-export { S as square};
-export { T as triangle};
