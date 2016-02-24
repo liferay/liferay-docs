3e56b23e93557f16b2416ad0f70900733de9e949
Jesse Rao <jesse.rao@liferay.com>
2016-02-24 18:08:41 -0500

    Add initial version of Sample Content portlet

---
+++ code/sample-content-portlet-lr-workspace/modules/com.liferay.docs.samplecontent/src/main/java/com/liferay/docs/samplecontent/portlet/SampleContentPortlet.java
@@ -0,0 +1,24 @@
+package com.liferay.docs.samplecontent.portlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=com.liferay.docs.samplecontent Portlet",
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language"
+	},
+	service = Portlet.class
+)
+public class SampleContentPortlet extends MVCPortlet {
+
+}
---
+++ code/sample-content-portlet-lr-workspace/modules/com.liferay.docs.samplecontent/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="com_liferay_docs_samplecontent_SampleContentPortlet.caption"/></b>
+</p>
