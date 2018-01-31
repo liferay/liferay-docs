52ab15e086527b86d1264a0fcb16374cf42bac4a
codyhoag <cody.hoag@liferay.com>
2018-01-31 14:49:23 -0500

    LRDOCS-3820 Build Services

--- develop/tutorials/code/com-liferay-docs-guestbook/modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/service/EntryLocalService.java
+++ develop/tutorials/code/com-liferay-docs-guestbook/modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/service/EntryLocalService.java
@@ -28,11 +28,13 @@ import com.liferay.portal.kernel.dao.orm.Projection;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.BaseLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Isolation;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
@@ -94,6 +96,7 @@ public interface EntryLocalService extends BaseLocalService,
 	* @return the entry that was removed
 	*/
 	@Indexable(type = IndexableType.DELETE)
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public Entry deleteEntry(Entry entry);
 
 	/**
