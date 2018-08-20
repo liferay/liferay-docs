dd5a577d74f1fdd81e49b22e5cf54d36d7d90ae4
Stephen Kostas <stephen.kostas@liferay.com>
2018-08-20 15:57:37 -0400

    added code to date for the project

---
+++ develop/tutorials/code/com-liferay-docs-guestbook/modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/service/EntryLocalService.java
@@ -0,0 +1,308 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.docs.guestbook.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.docs.guestbook.model.Entry;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Projection;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.BaseLocalService;
+import com.liferay.portal.kernel.service.PersistedModelLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.io.Serializable;
+
+import java.util.List;
+
+/**
+ * Provides the local service interface for Entry. Methods of this
+ * service will not have security checks based on the propagated JAAS
+ * credentials because this service can only be accessed from within the same
+ * VM.
+ *
+ * @author liferay
+ * @see EntryLocalServiceUtil
+ * @see com.liferay.docs.guestbook.service.base.EntryLocalServiceBaseImpl
+ * @see com.liferay.docs.guestbook.service.impl.EntryLocalServiceImpl
+ * @generated
+ */
+@ProviderType
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface EntryLocalService extends BaseLocalService,
+	PersistedModelLocalService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link EntryLocalServiceUtil} to access the entry local service. Add custom service methods to {@link com.liferay.docs.guestbook.service.impl.EntryLocalServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+
+	/**
+	* Adds the entry to the database. Also notifies the appropriate model listeners.
+	*
+	* @param entry the entry
+	* @return the entry that was added
+	*/
+	@Indexable(type = IndexableType.REINDEX)
+	public Entry addEntry(Entry entry);
+
+	public Entry addEntry(long userId, long guestbookId, String name,
+		String email, String message, ServiceContext serviceContext)
+		throws PortalException;
+
+	/**
+	* Creates a new entry with the primary key. Does not add the entry to the database.
+	*
+	* @param entryId the primary key for the new entry
+	* @return the new entry
+	*/
+	@Transactional(enabled = false)
+	public Entry createEntry(long entryId);
+
+	/**
+	* Deletes the entry from the database. Also notifies the appropriate model listeners.
+	*
+	* @param entry the entry
+	* @return the entry that was removed
+	*/
+	@Indexable(type = IndexableType.DELETE)
+	public Entry deleteEntry(Entry entry);
+
+	/**
+	* Deletes the entry with the primary key from the database. Also notifies the appropriate model listeners.
+	*
+	* @param entryId the primary key of the entry
+	* @return the entry that was removed
+	* @throws PortalException if a entry with the primary key could not be found
+	*/
+	@Indexable(type = IndexableType.DELETE)
+	public Entry deleteEntry(long entryId) throws PortalException;
+
+	public Entry deleteEntry(long entryId, ServiceContext serviceContext)
+		throws PortalException;
+
+	/**
+	* @throws PortalException
+	*/
+	@Override
+	public PersistedModel deletePersistedModel(PersistedModel persistedModel)
+		throws PortalException;
+
+	public DynamicQuery dynamicQuery();
+
+	/**
+	* Performs a dynamic query on the database and returns the matching rows.
+	*
+	* @param dynamicQuery the dynamic query
+	* @return the matching rows
+	*/
+	public <T> List<T> dynamicQuery(DynamicQuery dynamicQuery);
+
+	/**
+	* Performs a dynamic query on the database and returns a range of the matching rows.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.liferay.docs.guestbook.model.impl.EntryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param dynamicQuery the dynamic query
+	* @param start the lower bound of the range of model instances
+	* @param end the upper bound of the range of model instances (not inclusive)
+	* @return the range of matching rows
+	*/
+	public <T> List<T> dynamicQuery(DynamicQuery dynamicQuery, int start,
+		int end);
+
+	/**
+	* Performs a dynamic query on the database and returns an ordered range of the matching rows.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.liferay.docs.guestbook.model.impl.EntryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param dynamicQuery the dynamic query
+	* @param start the lower bound of the range of model instances
+	* @param end the upper bound of the range of model instances (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the ordered range of matching rows
+	*/
+	public <T> List<T> dynamicQuery(DynamicQuery dynamicQuery, int start,
+		int end, OrderByComparator<T> orderByComparator);
+
+	/**
+	* Returns the number of rows matching the dynamic query.
+	*
+	* @param dynamicQuery the dynamic query
+	* @return the number of rows matching the dynamic query
+	*/
+	public long dynamicQueryCount(DynamicQuery dynamicQuery);
+
+	/**
+	* Returns the number of rows matching the dynamic query.
+	*
+	* @param dynamicQuery the dynamic query
+	* @param projection the projection to apply to the query
+	* @return the number of rows matching the dynamic query
+	*/
+	public long dynamicQueryCount(DynamicQuery dynamicQuery,
+		Projection projection);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Entry fetchEntry(long entryId);
+
+	/**
+	* Returns the entry matching the UUID and group.
+	*
+	* @param uuid the entry's UUID
+	* @param groupId the primary key of the group
+	* @return the matching entry, or <code>null</code> if a matching entry could not be found
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Entry fetchEntryByUuidAndGroupId(String uuid, long groupId);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public ActionableDynamicQuery getActionableDynamicQuery();
+
+	/**
+	* Returns a range of all the entries.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.liferay.docs.guestbook.model.impl.EntryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param start the lower bound of the range of entries
+	* @param end the upper bound of the range of entries (not inclusive)
+	* @return the range of entries
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<Entry> getEntries(int start, int end);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<Entry> getEntries(long groupId, long guestbookId);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<Entry> getEntries(long groupId, long guestbookId, int start,
+		int end) throws SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<Entry> getEntries(long groupId, long guestbookId, int start,
+		int end, OrderByComparator<Entry> obc);
+
+	/**
+	* Returns all the entries matching the UUID and company.
+	*
+	* @param uuid the UUID of the entries
+	* @param companyId the primary key of the company
+	* @return the matching entries, or an empty list if no matches were found
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<Entry> getEntriesByUuidAndCompanyId(String uuid, long companyId);
+
+	/**
+	* Returns a range of entries matching the UUID and company.
+	*
+	* @param uuid the UUID of the entries
+	* @param companyId the primary key of the company
+	* @param start the lower bound of the range of entries
+	* @param end the upper bound of the range of entries (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the range of matching entries, or an empty list if no matches were found
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<Entry> getEntriesByUuidAndCompanyId(String uuid,
+		long companyId, int start, int end,
+		OrderByComparator<Entry> orderByComparator);
+
+	/**
+	* Returns the number of entries.
+	*
+	* @return the number of entries
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getEntriesCount();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getEntriesCount(long groupId, long guestbookId);
+
+	/**
+	* Returns the entry with the primary key.
+	*
+	* @param entryId the primary key of the entry
+	* @return the entry
+	* @throws PortalException if a entry with the primary key could not be found
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Entry getEntry(long entryId) throws PortalException;
+
+	/**
+	* Returns the entry matching the UUID and group.
+	*
+	* @param uuid the entry's UUID
+	* @param groupId the primary key of the group
+	* @return the matching entry
+	* @throws PortalException if a matching entry could not be found
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Entry getEntryByUuidAndGroupId(String uuid, long groupId)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		PortletDataContext portletDataContext);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public IndexableActionableDynamicQuery getIndexableActionableDynamicQuery();
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public String getOSGiServiceIdentifier();
+
+	@Override
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public PersistedModel getPersistedModel(Serializable primaryKeyObj)
+		throws PortalException;
+
+	/**
+	* Updates the entry in the database or adds it if it does not yet exist. Also notifies the appropriate model listeners.
+	*
+	* @param entry the entry
+	* @return the entry that was updated
+	*/
+	@Indexable(type = IndexableType.REINDEX)
+	public Entry updateEntry(Entry entry);
+
+	public Entry updateEntry(long userId, long guestbookId, long entryId,
+		String name, String email, String message, ServiceContext serviceContext)
+		throws PortalException, SystemException;
+}
---
+++ develop/tutorials/code/com-liferay-docs-guestbook/modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/GuestbookAdminPortlet.java
@@ -0,0 +1,131 @@
+package com.liferay.docs.guestbook.portlet;
+
+import com.liferay.docs.guestbook.model.Guestbook;
+import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
+import com.liferay.docs.guestbook.service.GuestbookLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+        immediate = true,
+        property = {
+                "com.liferay.portlet.display-category=category.hidden",
+                "com.liferay.portlet.scopeable=true",
+                "javax.portlet.display-name=Guestbooks",
+                "javax.portlet.expiration-cache=0",
+                "javax.portlet.init-param.portlet-title-based-navigation=true",
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/guestbookadminportlet/view.jsp",
+                "javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK_ADMIN,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=administrator",
+                "javax.portlet.supports.mime-type=text/html",
+                "com.liferay.portlet.add-default-resource=true"
+        },
+        service = Portlet.class
+    )
+
+
+public class GuestbookAdminPortlet extends MVCPortlet {
+	
+	public void addGuestbook(ActionRequest request, ActionResponse response)
+            throws PortalException {
+
+            ServiceContext serviceContext = ServiceContextFactory.getInstance(
+                Guestbook.class.getName(), request);
+
+            String name = ParamUtil.getString(request, "name");
+
+            try {
+                _guestbookLocalService.addGuestbook(
+                    serviceContext.getUserId(), name, serviceContext);
+                
+                SessionMessages.add(request, "guestbookAdded");
+
+            }
+            catch (PortalException pe) {
+
+                Logger.getLogger(GuestbookAdminPortlet.class.getName()).log(
+                    Level.SEVERE, null, pe);
+
+        		SessionErrors.add(request, pe.getClass().getName());
+                
+                response.setRenderParameter(
+                    "mvcPath", "/guestbookadminportlet/edit_guestbook.jsp");
+            }
+        }
+        
+        private GuestbookLocalService _guestbookLocalService;
+        
+        @Reference(unbind = "-")
+        protected void setGuestbookService(GuestbookLocalService guestbookLocalService) {
+            _guestbookLocalService = guestbookLocalService;
+        }	
+        
+        public void updateGuestbook(ActionRequest request, ActionResponse response)
+                throws PortalException {
+
+                ServiceContext serviceContext = ServiceContextFactory.getInstance(
+                    Guestbook.class.getName(), request);
+
+                String name = ParamUtil.getString(request, "name");
+                long guestbookId = ParamUtil.getLong(request, "guestbookId");
+
+                try {
+                    _guestbookLocalService.updateGuestbook(
+                        serviceContext.getUserId(), guestbookId, name, serviceContext);
+                    
+                    SessionMessages.add(request, "guestbookUpdated");
+
+
+                } catch (PortalException pe) {
+                
+                    Logger.getLogger(GuestbookAdminPortlet.class.getName()).log(
+                        Level.SEVERE, null, pe);
+                    
+            		SessionErrors.add(request, pe.getClass().getName());
+
+                    response.setRenderParameter(
+                        "mvcPath", "/guestbookadminportlet/edit_guestbook.jsp");
+                }
+            }
+        
+        public void deleteGuestbook(ActionRequest request, ActionResponse response)
+                throws PortalException {
+
+                ServiceContext serviceContext = ServiceContextFactory.getInstance(
+                    Guestbook.class.getName(), request);
+
+                long guestbookId = ParamUtil.getLong(request, "guestbookId");
+
+                try {
+                    _guestbookLocalService.deleteGuestbook(guestbookId, serviceContext);
+                    
+                    SessionMessages.add(request, "guestbookDeleted");
+
+                }
+                catch (PortalException pe) {
+
+                    Logger.getLogger(GuestbookAdminPortlet.class.getName()).log(
+                        Level.SEVERE, null, pe);
+            		SessionErrors.add(request, pe.getClass().getName());
+
+                }
+            }
+
+}
