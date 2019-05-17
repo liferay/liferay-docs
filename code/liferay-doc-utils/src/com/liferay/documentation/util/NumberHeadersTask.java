0e5b758bb8a08fd0b4e4cbfae5c8251a7e85190e
codyhoag <cody.hoag@liferay.com>
2019-05-17 11:39:53 -0400

    LRDOCS-6856 Ensure header syntax is only validated once

--- code/liferay-doc-utils/src/com/liferay/documentation/util/NumberHeadersTask.java
+++ code/liferay-doc-utils/src/com/liferay/documentation/util/NumberHeadersTask.java
@@ -91,11 +91,12 @@ public class NumberHeadersTask extends Task {
 
 					String line;
 					int titleHeaderLineNum = -2;
+					boolean headerValidated = false;
 					while ((line = in.readLine()) != null) {
 
 						int lineNum = in.getLineNumber();
 
-						if (line.startsWith("#") && !line.startsWith("##")) {
+						if (line.startsWith("#") && !line.startsWith("##") && !headerValidated) {
 						
 							titleHeaderLineNum = in.getLineNumber();
 							line = line.trim();
@@ -113,6 +114,8 @@ public class NumberHeadersTask extends Task {
 							else {
 								validateHeaderId(filename, headerIdLine, in.getLineNumber(), true);
 							}
+
+							headerValidated = true;
 						}
 						if (lineNum == titleHeaderLineNum + 1) {
 							if (!line.equals("")) {
