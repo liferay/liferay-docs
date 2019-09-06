96c9e83e1e6b7d944fc5f8e7fb980a6b0d442904
Rich Sezov <richard.sezov@liferay.com>
2019-09-06 11:41:31 -0400

    LRDOCS-7240 Source code for Admin Portlet.

---
+++ developer/tutorials/code/guestbook/05-admin-portlet/com-liferay-docs-guestbook/modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/http/GuestbookEntryServiceHttp.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.docs.guestbook.service.http;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * Provides the HTTP utility for the
+ * <code>com.liferay.docs.guestbook.service.GuestbookEntryServiceUtil</code> service
+ * utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it requires an additional
+ * <code>com.liferay.portal.kernel.security.auth.HttpPrincipal</code> parameter.
+ *
+ * <p>
+ * The benefits of using the HTTP utility is that it is fast and allows for
+ * tunneling without the cost of serializing to text. The drawback is that it
+ * only works with Java.
+ * </p>
+ *
+ * <p>
+ * Set the property <b>tunnel.servlet.hosts.allowed</b> in portal.properties to
+ * configure security.
+ * </p>
+ *
+ * <p>
+ * The HTTP utility is only generated for remote services.
+ * </p>
+ *
+ * @author Liferay
+ * @see GuestbookEntryServiceSoap
+ * @generated
+ */
+@ProviderType
+public class GuestbookEntryServiceHttp {
+}
---
+++ developer/tutorials/code/guestbook/05-admin-portlet/com-liferay-docs-guestbook/modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/http/GuestbookEntryServiceSoap.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.docs.guestbook.service.http;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * Provides the SOAP utility for the
+ * <code>com.liferay.docs.guestbook.service.GuestbookEntryServiceUtil</code> service
+ * utility. The static methods of this class call the same methods of the
+ * service utility. However, the signatures are different because it is
+ * difficult for SOAP to support certain types.
+ *
+ * <p>
+ * ServiceBuilder follows certain rules in translating the methods. For example,
+ * if the method in the service utility returns a <code>java.util.List</code>,
+ * that is translated to an array of
+ * <code>com.liferay.docs.guestbook.model.GuestbookEntrySoap</code>. If the method in the
+ * service utility returns a
+ * <code>com.liferay.docs.guestbook.model.GuestbookEntry</code>, that is translated to a
+ * <code>com.liferay.docs.guestbook.model.GuestbookEntrySoap</code>. Methods that SOAP
+ * cannot safely wire are skipped.
+ * </p>
+ *
+ * <p>
+ * The benefits of using the SOAP utility is that it is cross platform
+ * compatible. SOAP allows different languages like Java, .NET, C++, PHP, and
+ * even Perl, to call the generated services. One drawback of SOAP is that it is
+ * slow because it needs to serialize all calls into a text format (XML).
+ * </p>
+ *
+ * <p>
+ * You can see a list of services at http://localhost:8080/api/axis. Set the
+ * property <b>axis.servlet.hosts.allowed</b> in portal.properties to configure
+ * security.
+ * </p>
+ *
+ * <p>
+ * The SOAP utility is only generated for remote services.
+ * </p>
+ *
+ * @author Liferay
+ * @see GuestbookEntryServiceHttp
+ * @generated
+ */
+@ProviderType
+public class GuestbookEntryServiceSoap {
+}
