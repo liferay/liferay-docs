8da01924b5d4441ec0505c1d3f0adf56c1132d71
Rich Sezov <richard.sezov@liferay.com>
2019-09-10 19:46:53 -0400

    Modify convert script to work with new class.

--- bin/convert.sh
+++ bin/convert.sh
@@ -19,10 +19,12 @@ elif [ $# -lt 2 ]; then
 	htmlFile=$1
 	htmlFile=$(echo "${htmlFile}" | sed 's/[^.]*$//')
 	htmlFile=${htmlFile}html
-	echo "java -cp "${classpathDir}/*" com.liferay.documentation.util.MarkdownToHtml $1 ${htmlFile}"
-	java -cp "${classpathDir}/*" com.liferay.documentation.util.MarkdownToHtml $1 ${htmlFile}
+	#echo "java -cp "${classpathDir}/*" com.liferay.documentation.util.MarkdownToHtml $1 ${htmlFile}"
+	#java -cp "${classpathDir}/*" com.liferay.documentation.util.MarkdownToHtml $1 ${htmlFile}
+	echo "java -cp "${classpathDir}/*" com.liferay.knowledge.base.markdown.converter.cli.MarkdownConverterCLI $1 > ${htmlFile}"
+	java -cp "${classpathDir}/*" com.liferay.knowledge.base.markdown.converter.cli.MarkdownConverterCLI $1 > ${htmlFile}
 	exit 0
 else
-	java -cp "${classpathDir}/*" com.liferay.documentation.util.MarkdownToHtml $1 $2
+	java -cp "${classpathDir}/*" com.liferay.documentation.util.MarkdownToHtml $1 > $2
 	exit 0
 fi
